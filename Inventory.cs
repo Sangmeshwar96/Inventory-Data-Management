7b56e1565fc3e5af8e241edccc4320fd2cc86d6c
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-12-11 11:09:16 +0530

    Welcome to InventoryManagement System With JSON

---
+++ Inventory.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+
+namespace Data_Inventory_Management
+{
+
+    public class Inventory
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public double Price { get; set; }
+    }
+
+    public class Root
+    {
+        public List<Inventory> inventories { get; set; }
+    }
+}
