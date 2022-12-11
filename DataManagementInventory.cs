6738a19e7dac7bc2d1567c0d7b0e980a57848510
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-12-11 11:07:17 +0530

    Using OOPs & JSON Creating InventoryDataManagement

---
+++ DataManagementInventory.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Data_Inventory_Management
+{
+    public class DataInventortyManagement
+    {
+
+        public void GetData()
+        {
+            string filepath = @"C:\Users\hp\source\repos\DataInventoryManagement\DataInventoryManagement\DataInventory.json";
+            try
+            {
+                if (File.Exists(filepath))
+                {
+                    string inverntoryData = File.ReadAllText(filepath);
+                    Root root = JsonConvert.DeserializeObject<Root>(inverntoryData);
+                    List<Inventory> inventories = root.inventories;
+                    foreach (Inventory inventory in inventories)
+                    {
+                        Console.WriteLine($" Name: {inventory.Name} \n Weight: {inventory.Weight} \n Price: {inventory.Price} \n");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("File Not found");
+                    throw new Exception();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Data_Inventory_Management
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("---Welcome To DataManagement Inventory Using JSON---");
+            Console.WriteLine(" ");
+            DataInventortyManagement management = new DataInventortyManagement();
+            management.GetData();
+
+        }
+    }
+}
